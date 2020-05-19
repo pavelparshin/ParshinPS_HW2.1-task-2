82765c0a96064b03551161d4bc2a830335e9f282
Pavel Parshin <mail@pavelparshin.ru>
2020-05-19 23:03:01 +0300

    work with interface

--- ParshinPS_HW2.1 task 2/ViewController.swift
+++ ParshinPS_HW2.1 task 2/ViewController.swift
@@ -10,11 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redLightView: UIView!
+    @IBOutlet weak var yellowLightView: UIView!
+    @IBOutlet weak var greenLightView: UIView!
+    @IBOutlet weak var switchLightButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func switchLightButtonPressed() {
+        
+        
+    }
+    
 }
 
