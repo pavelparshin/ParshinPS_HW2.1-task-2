3d2f0c3203090873810ee122036cdec6c52f855d
Pavel Parshin <mail@pavelparshin.ru>
2020-05-19 19:32:52 +0300

    Initial Commit

---
+++ ParshinPS_HW2.1 task 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ParshinPS_HW2.1 task 2
+//
+//  Created by Pavel Parshin on 19.05.2020.
+//  Copyright © 2020 Pavel Parshin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
